3deb53675fd7d585dc04fe060279604797f22ef9
mohammad <mohammad-bayat-1381@outlook.com>
2022-04-20 14:43:17 -0700

    finishing city class

---
+++ civilization/src/main/java/model/City.java
@@ -0,0 +1,219 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class City {
+    private String name;
+    private Tile cityLocation;
+    private User ownerShip;
+    private ArrayList<Tile> ownerShipTiles;
+    private int HP;
+    private int defence;
+    private Unit garrison;
+    private ArrayList<Product> products;
+    private int productTurnLeft;
+    private int citizens;
+    private int food;
+    private int production;
+    private int gold;
+    private int science;
+    private int faith;
+    private int tourism;
+    private int culture;
+    private HashMap<Integer, Tile> citizensLocation;
+    private ArrayList<Building> buildings;
+    private ArrayList<Unit> units;
+    private boolean warStatus;
+    private ArrayList<Citizen> expertCitizens;
+
+
+    public City(String name, Tile cityLocation, User ownerShip, ArrayList<Tile> ownerShipTiles, int HP, int defence, Unit garrison,
+                ArrayList<Product> products, int productTurnLeft, int citizens, int food, int production, int gold, int science, int faith,
+                int tourism, int culture, HashMap<Integer, Tile> citizensLocation, ArrayList<Building> buildings, ArrayList<Unit> units,
+                boolean warStatus, ArrayList<Citizen> expertCitizens) {
+        this.name = name;
+        this.cityLocation = cityLocation;
+        this.ownerShip = ownerShip;
+        this.ownerShipTiles = ownerShipTiles;
+        this.HP = HP;
+        this.defence = defence;
+        this.garrison = garrison;
+        this.products = products;
+        this.productTurnLeft = productTurnLeft;
+        this.citizens = citizens;
+        this.food = food;
+        this.production = production;
+        this.gold = gold;
+        this.science = science;
+        this.faith = faith;
+        this.tourism = tourism;
+        this.culture = culture;
+        this.citizensLocation = citizensLocation;
+        this.buildings = buildings;
+        this.units = units;
+        this.warStatus = warStatus;
+        this.expertCitizens = expertCitizens;
+    }
+
+    public Tile getCityLocation() {
+        return this.cityLocation;
+    }
+
+    public void addOwnerShipLand(Tile tile) {
+        this.ownerShipTiles.add(tile);
+    }
+
+    public ArrayList<Tile> getOwnerShipLands() {
+        return this.ownerShipTiles;
+    }
+
+    public void setDefence(int amount) {
+        this.defence += amount;
+    }
+
+    public int getDefence() {
+        return this.defence;
+    }
+
+    public void setGarrison(Unit garrison) {
+        this.garrison = garrison;
+    }
+
+    public Unit getGarrison() {
+        return this.garrison;
+    }
+
+    public void setProduct(Product product) {
+        this.products.add(product);
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProductTurnLeft(int turn) {
+        this.productTurnLeft = turn;
+    }
+
+    public int getProductTurnLeft() {
+        return productTurnLeft;
+    }
+
+    public void setCitizens(int amount) {
+        this.citizens = amount;
+    }
+
+    public int getCitizens() {
+        return citizens;
+    }
+
+    public void setFood(int amount) {
+        this.food = amount;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setProduction(int amount) {
+        this.production = amount;
+    }
+
+    public int getProduction() {
+        return production;
+    }
+
+    public void setGold(int amount) {
+        this.gold = amount;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setScience(int amount) {
+        this.science = amount;
+    }
+
+    public int getScience() {
+        return science;
+    }
+
+    public void setFaith(int amount) {
+        this.faith = amount;
+    }
+
+    public int getFaith() {
+        return faith;
+    }
+
+    public void setTourism(int amount) {
+        this.tourism = amount;
+    }
+
+    public int getTourism() {
+        return tourism;
+    }
+
+    public void setCulture(int amount) {
+        this.culture = amount;
+    }
+
+    public int getCulture() {
+        return culture;
+    }
+
+    public void addCitizensLocation(int number, Tile tile) {
+        this.citizensLocation.put(number, tile);
+    }
+
+    public HashMap<Integer, Tile> getCitizensLocation() {
+        return citizensLocation;
+    }
+
+    public void addBuildings(Building building) {
+        this.buildings.add(building);
+    }
+
+    public ArrayList<Building> getBuildings() {
+        return buildings;
+    }
+
+    public void addUnit(Unit unit) {
+        this.units.add(unit);
+    }
+
+    public ArrayList<Unit> getUnits() {
+        return units;
+    }
+
+    public void setWarStatus(boolean status) {
+        this.warStatus = status;
+    }
+
+    public boolean getWarStatus() {
+        return warStatus;
+    }
+
+    public void addExpertCitizen(Citizen citizen) {
+        this.expertCitizens.add(citizen);
+    }
+
+    public ArrayList<Citizen> getExpertCitizens() {
+        return expertCitizens;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setOwnerShip(User ownerShip) {
+        this.ownerShip = ownerShip;
+    }
+
+}
