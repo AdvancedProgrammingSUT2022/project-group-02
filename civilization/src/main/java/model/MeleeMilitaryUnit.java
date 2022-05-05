94bc9409dce4fa1b2a8616985c236f39f1884a01
shayanshabani <shayan.shabani5814@gmail.com>
2022-05-05 15:55:43 +0430

    commit .class files

--- civilization/src/main/java/model/MeleeMilitaryUnit.java
+++ civilization/src/main/java/model/MeleeMilitaryUnit.java
@@ -3,7 +3,7 @@ package model;
 public class MeleeMilitaryUnit extends MilitaryUnit{
 
     public MeleeMilitaryUnit(String name, Tile tile, int HP, int price, int level, int MP, int combatStrength, int rangeCombatStrength, User user) {
-        super(name, tile, HP, price, level, MP, combatStrength, rangeCombatStrength, user);
+        super(name, tile, HP, price, level, MP, combatStrength, rangeCombatStrength, user, combatStrength);
     }
 
 
