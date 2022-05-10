aa30fe5e51c16bc19c89b97d7f5ed96fb8b972ff
shayanshabani <shayan.shabani5814@gmail.com>
2022-05-10 08:44:27 +0430

    handle a bug in assigning settlers

--- civilization/src/main/java/model/Settler.java
+++ civilization/src/main/java/model/Settler.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 public class Settler extends Civilian{
 
     private ArrayList<Tile> possibleTilesForCities;
-    public Settler(String name, Tile tile, int HP, int price, int level, int MP, int combatStrength, int rangeCombatStrength, ArrayList<Tile> possibleTilesForCities, User user, int attackPoint) {
-        super(name, tile, HP, price, level, MP, combatStrength, rangeCombatStrength, user, attackPoint);
+    public Settler(String name, Tile tile, int HP, int goldPrice, int productionPrice, int level, int MP, int combatStrength, int rangeCombatStrength, ArrayList<Tile> possibleTilesForCities, User user, int attackPoint, int maintainGold) {
+        super(name, tile, HP, goldPrice, productionPrice, level, MP, combatStrength, rangeCombatStrength, user, attackPoint, maintainGold);
         this.possibleTilesForCities = possibleTilesForCities;
 
     }
