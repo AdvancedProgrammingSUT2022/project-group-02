9fae3e07fb0ecf70396f0f5f8ff05bd5a9799dd9
mohammad bayat <mohammad-bayat-1381@outlook.com>
2022-05-15 11:53:18 +0430

    first steps in unit tests

--- civilization/src/test/java/controller/CombatControllerTest.java
+++ civilization/src/test/java/controller/CombatControllerTest.java
@@ -2,31 +2,57 @@ package controller;
 
 import model.Terrain;
 import model.Tile;
+import model.Unit;
 import model.User;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class CombatControllerTest {
-    @Before
-    public void setUp() throws Exception {
-    }
 
-    @After
-    public void tearDown() throws Exception {
+    @Mock
+    Tile tile;
+    @Mock
+    Terrain Hill;
+    @Mock
+    User attackerUser;
+    @Mock
+    Tile selectedTileForAttack;
+    @Mock
+    Tile selectedTileForDefend;
+    @Mock
+    Unit militaryUnit;
+
+    @Before
+    public void setUp() {
+
     }
 
     @Test
     public void checkDefenderHill() {
-        Terrain Hill = new Terrain("Hill", "purple", 2, 0, 0.25, 0, 2, true);
-        Tile tile = new Tile(18, 20, null, Hill, 0, false, null, null, null);
+//        Terrain Hill = new Terrain("Hill", "purple", 2, 0, 0.25, 0, 2, true);
+//        Tile tile = new Tile(18, 20, null, Hill, 0, false, null, null, null);
+        when(tile.getTerrain()).thenReturn(Hill);
+        when(Hill.getName()).thenReturn("Hill");
+
         Assert.assertTrue(true);
     }
 
     @Test
     public void combatHandler() {
-        User attackerUser = Mockito.mock(User.class);
+//        User attackerUser = Mockito.mock(User.class);
+
     }
 
     @Test
