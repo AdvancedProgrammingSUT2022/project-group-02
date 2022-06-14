717a36bc84b7f7fcea58f295a8a7c90df0a86ecf
shayanshabani <shayan.shabani5814@gmail.com>
2022-06-14 08:35:45 +0430

    buildings added to research panel

--- civilization-in-javafx/src/main/java/controller/TechController.java
+++ civilization-in-javafx/src/main/java/controller/TechController.java
@@ -6,28 +6,28 @@ import model.User;
 import java.util.ArrayList;
 
 public class TechController {
-    private final ArrayList<Technology> ancientTechnologies;
-    private final int[][] ancientEraTechnologiesGraph;
+    private final ArrayList<Technology> technologies;
+    private final int[][] technologiesGraph;
 
     public TechController(int[][] ancientEraTechnologiesGraph, ArrayList<Technology> ancientTechnologies) {
-        this.ancientEraTechnologiesGraph = ancientEraTechnologiesGraph;
-        this.ancientTechnologies = ancientTechnologies;
+        this.technologiesGraph = ancientEraTechnologiesGraph;
+        this.technologies = ancientTechnologies;
     }
 
-    public ArrayList<Technology> getAncientTechnologies() {
-        return ancientTechnologies;
+    public ArrayList<Technology> getTechnologies() {
+        return technologies;
     }
 
 //    public int[][] getAncientEraTechnologiesGraph() {
 //        return ancientEraTechnologiesGraph;
 //    }
 
-    public ArrayList<Technology> getPrerequisitesAncientTech(Technology technology) {
+    public ArrayList<Technology> getPrerequisitesTech(Technology technology) {
         int index = technology.getIndex();
         ArrayList<Technology> prerequisites = new ArrayList<>();
-        for (int i = 0; i < ancientTechnologies.size(); i++) {
-            if (ancientEraTechnologiesGraph[i][index] == 1) {
-                prerequisites.add(ancientTechnologies.get(i));
+        for (int i = 0; i < technologies.size(); i++) {
+            if (technologiesGraph[i][index] == 1) {
+                prerequisites.add(technologies.get(i));
             }
         }
         return prerequisites;
@@ -38,10 +38,10 @@ public class TechController {
         ArrayList<Technology> userTechnologies = user.getTechnologies();
         ArrayList<Technology> prerequisites;
         boolean treeTech = true;
-        for (Technology ancientTechnology : ancientTechnologies) {
+        for (Technology ancientTechnology : technologies) {
             // first check user do not have this
             if (!userTechnologies.contains(ancientTechnology)) {
-                prerequisites = getPrerequisitesAncientTech(ancientTechnology);
+                prerequisites = getPrerequisitesTech(ancientTechnology);
                 if (prerequisites != null) {
                     // check if all prerequisites are exists in user technologies
                     for (int i = 0; i < prerequisites.size(); i++) {
